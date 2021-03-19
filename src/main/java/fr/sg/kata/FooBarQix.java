99c2398d60140daafd1aaeaf9d54dce1db35eb65
hiba zemni <hibazemni@gmail.com>
2021-03-19 22:57:11 +0100

    Refactoring code

--- src/main/java/fr/sg/kata/FooBarQix.java
+++ src/main/java/fr/sg/kata/FooBarQix.java
@@ -8,25 +8,30 @@ public class FooBarQix {
         StringBuilder sb = new StringBuilder();
         for (Integer number : numbers) {
             sb = sb.append(evaluateDivisibleRules(number));
-            for (char c : String.valueOf(number).toCharArray()) {
-                if (c == '3'){
-                    sb = sb.append("Foo");
-                }
-            }
+            sb = evaluateContainsRules(sb, number);
 
         }
         return sb.toString();
     }
 
-    private String evaluateDivisibleRules (Integer number) {
+    private StringBuilder evaluateContainsRules(StringBuilder sb, Integer number) {
+        for (char c : String.valueOf(number).toCharArray()) {
+            if (c == '3'){
+                sb = sb.append("Foo");
+            }
+        }
+        return sb;
+    }
+
+    private StringBuilder evaluateDivisibleRules (Integer number) {
         StringBuilder sb = new StringBuilder();
         if (number % 3 == 0)
             sb = sb.append("Foo");
         if (number  % 5 == 0)
             sb = sb.append("Bar");
         if (sb.length() == 0)
-            return String.valueOf(number);
-        return sb.toString();
+            return sb.append(number.toString());
+        return sb;
 
     }
 }
