fda37132c08d98969fb4c728ee5e1ad572a02ee9
hiba zemni <hibazemni@gmail.com>
2021-03-19 20:56:57 +0100

    Running test with minimum code

--- src/main/java/fr/sg/kata/FooBarQix.java
+++ src/main/java/fr/sg/kata/FooBarQix.java
@@ -5,6 +5,8 @@ public class FooBarQix {
     public String transformNumbers(int number){
         if (number % 3 == 0)
             return "Foo";
+        if (number  == 5)
+            return "Bar";
         return String.valueOf(number);
 
     }
